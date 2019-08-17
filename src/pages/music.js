47abc193887b75cd5a1117e97059b15ce07b75cb
Pete DeJoy <pete@astronomer.io>
2019-08-17 14:38:28 -0400

    Change music import

--- src/pages/music.js
+++ src/pages/music.js
@@ -1,9 +1,8 @@
 import React from "react"
 import Layout from "../components/layout"
-import PostList from "../components/PostList"
 import { Link } from "gatsby"
 import { graphql, useStaticQuery } from "gatsby"
-import Music from '../components/music'
+import Music from '../components/Music'
 
 export default () => {
 
