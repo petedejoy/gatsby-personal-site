bb3b396b14c0cbd73811afe19671bf1e645488d0
petedejoy <pete@astronomer.io>
2020-08-09 22:12:34 -0400

    Mailing list

---
+++ src/components/MailingList.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import classnames from "classnames"
+import styles from "../css/mailingList.module.css"
+
+const MailingList = () => {
+  return (
+    <div className={styles.container}>
+      <form className={styles.form} name="mailing" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" />
+        <div className={classnames(styles.padding)}>
+          <p className={classnames(styles.label)}>
+             <input type="email" name="email" placeholder="Email" className={styles.input} />
+          </p>
+        </div>
+        <div className={classnames(styles.padding)}>
+          <p>
+            <button className={styles.button} type="submit">Subscribe for Updates</button>
+          </p>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default MailingList
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,8 +1,7 @@
 import React from "react"
-import PropTypes from "prop-types"
 import PostCard from "./PostCard"
+import MailingList from "./MailingList"
 import styles from "../css/postlist.module.css"
-import pk from "../images/pk2.jpg"
 import { Helmet } from "react-helmet"
 import writingMeta from "../images/writing-meta.png"
 
@@ -11,14 +10,15 @@ const PostList = ({ posts }) => {
     <section>
       <Helmet>
         <meta charSet='utf-8' />
-        <meta property="og:title" content="Pete DeJoy | Writing"/>
-        <meta property="og:description" content="Musings on technology, business, music, mental health, and everything in-between."/>
+        <meta property="og:title" content="Pete DeJoy | Writing" />
+        <meta property="og:description" content="Musings on technology, business, music, mental health, and everything in-between." />
         <meta property="og:image" content={`https://${process.env.GATSBY_DOMAIN}.com${writingMeta}`} />
         <meta property="og:url" content={`https://${process.env.GATSBY_DOMAIN}.com/writing`} />
         <meta name="twitter:image" content={`https://${process.env.GATSBY_DOMAIN}.com${writingMeta}`} />
         <meta name="twitter:card" content='summary_large_image' />
       </Helmet>
       <h1 className="intro">Technology, business, music, mental health, and everything in-between.</h1>
+      <MailingList/>
       <div className={styles.center}>
         {posts.map(({ node }, index) => {
           return <PostCard key={index} post={node} />
