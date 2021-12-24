c7444bb0cf7cf785fc5d1b18f1c5744c8483c7a5
petedejoy <pete@astronomer.io>
2021-12-24 15:11:08 -0500

    copy change

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -17,7 +17,7 @@ const PostList = ({ posts }) => {
         <meta name="twitter:image" content={`https://${process.env.GATSBY_DOMAIN}.com${writingMeta}`} />
         <meta name="twitter:card" content='summary_large_image' />
       </Helmet>
-      <h1 className="intro">Technology, business, music, mental health, and everything in-between.</h1>
+      <h1 className="intro">Musings on harebrained ideas.</h1>
       <div className={styles.center}>
         {posts.map(({ node }, index) => {
           return <PostCard key={index} post={node} />
